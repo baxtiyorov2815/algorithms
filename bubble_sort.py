4d65954f90dbf68ad0490b7b35797c7da614f82b
unknown <dbaxtiyorov35@gmail.com>
2024-11-07 04:22:50 -0800

    bubble and quick sort

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+# O(n^2)
+def bubblesort(arr, sorted=False):
+
+    while not sorted:
+        sorted = True
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                sorted = False
+
+    return arr
+
+arr = [5, 3, 50, 68, 12]
+
+print(bubblesort(arr))
